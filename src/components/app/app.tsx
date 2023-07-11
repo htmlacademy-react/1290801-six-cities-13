60fb3058d4a05e51d02eb9c8b1db78dc7fd2186c
Alexey Chernykh <alexeyshockman@gmail.com>
2023-07-11 21:26:54 +0300

    added page markup

---
+++ src/components/app/app.tsx
@@ -0,0 +1,23 @@
+import MainPage from "../../pages/main-page";
+import FavoritesPage from '../../pages/favorites-page';
+import FavoritesEmptyPage from '../../pages/favorites-empty-page';
+import LoginPage from '../../pages/login-page';
+import MainEmptyPage from '../../pages/main-empty-page';
+import OfferPage from '../../pages/offer-page';
+import OfferNotLoggedPage from '../../pages/offer-not-logged-page';
+
+function App(): JSX.Element {
+	return (
+		<>
+			<MainPage />
+			{/*<FavoritesPage />*/}
+			{/*<FavoritesEmptyPage />*/}
+			{/*<LoginPage />*/}
+			{/*<MainEmptyPage />*/}
+			{/*<OfferNotLoggedPage />*/}
+			{/*<OfferPage />*/}
+		</>
+	);
+}
+
+export default App;
