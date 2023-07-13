b95e1d4cfd7653ead20fbbd447358609499652ec
Alexey Shockman <alexeyshockman@gmail.com>
2023-07-13 21:50:01 +0400

    Начинаем программировать (#2)
    * added page markup
    
    * fixed spaces on pages
    
    * passed a prop to the offer component
    
    * added favicon
    
    * lint fix

---
+++ src/components/app/app.tsx
@@ -0,0 +1,27 @@
+import MainPage from '../../pages/main-page';
+// import FavoritesPage from '../../pages/favorites-page';
+// import FavoritesEmptyPage from '../../pages/favorites-empty-page';
+// import LoginPage from '../../pages/login-page';
+// import MainEmptyPage from '../../pages/main-empty-page';
+// import OfferPage from '../../pages/offer-page';
+// import OfferNotLoggedPage from '../../pages/offer-not-logged-page';
+
+type AppProps = {
+	numbersOfOffers: number;
+}
+
+function App({numbersOfOffers} : AppProps): JSX.Element {
+	return (
+		<>
+			<MainPage numbersOfOffers={numbersOfOffers}/>
+			{/*<FavoritesPage />*/}
+			{/*<FavoritesEmptyPage />*/}
+			{/*<LoginPage />*/}
+			{/*<MainEmptyPage />*/}
+			{/*<OfferNotLoggedPage />*/}
+			{/*<OfferPage />*/}
+		</>
+	);
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './components/app/app';
+
+import NUMBERS_OF_OFFERS from './consts';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 
+
 root.render(
 	<React.StrictMode>
-		<h1>Hello, World!</h1>
+		<App numbersOfOffers={NUMBERS_OF_OFFERS}/>
 	</React.StrictMode>
 );
