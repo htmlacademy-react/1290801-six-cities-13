2dfb6e82c89bfed697867b73f7dc65ace41d6c8c
Alexey Chernykh <alexeyshockman@gmail.com>
2023-07-15 14:23:10 +0300

    add private routing to favorites page

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,12 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import {AppRoute} from '../../consts';
+import {AppRoute, AuthorizationStatus} from '../../consts';
 
 import MainPage from '../../pages/main-page';
 import LoginPage from '../../pages/login-page';
-import FavoritesPage from '../../pages/favorites-page';
 import NotFoundPage from '../../pages/not-found-page/not-found-page';
 import OfferPage from '../../pages/offer-page';
+import PrivateRoute from '../private-route/private-route';
+import FavoritesPage from '../../pages/favorites-page';
 
 type AppProps = {
 	numbersOfOffers: number;
@@ -17,23 +18,20 @@ function App({numbersOfOffers} : AppProps): JSX.Element {
 			<Routes>
 				<Route path={AppRoute.Main} element={<MainPage numbersOfOffers={numbersOfOffers} />} />
 				<Route path={AppRoute.Login} element={<LoginPage />} />
-				<Route path={AppRoute.Favorites} element={<FavoritesPage />} />
+				<Route
+					path={AppRoute.Favorites}
+					// element={<FavoritesPage />}
+					element={
+						<PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+							<FavoritesPage />
+						</PrivateRoute>
+					}
+				/>
 				<Route path={AppRoute.Offer} element={<OfferPage />} />
 				<Route path ='*' element={<NotFoundPage />} />
 			</Routes>
 		</BrowserRouter>
 	);
-	// return (
-	// 	<>
-	// 		<MainPage numbersOfOffers={numbersOfOffers}/>
-	// 		{/*<FavoritesPage />*/}
-	// 		{/*<FavoritesEmptyPage />*/}
-	// 		{/*<LoginPage />*/}
-	// 		{/*<MainEmptyPage />*/}
-	// 		{/*<OfferNotLoggedPage />*/}
-	// 		{/*<OfferPage />*/}
-	// 	</>
-	// );
 }
 
 export default App;
--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,13 @@ export const NUMBERS_OF_OFFERS = 5;
 
 export enum AppRoute {
 	Main = '/',
-	Login = 'login',
+	Login = '/login',
 	Favorites = '/favorites',
 	Offer = '/offer/:id',
+}
+
+export enum AuthorizationStatus {
+	Auth = 'AUTH',
+	NoAuth = 'NO_AUTH',
+	Unknown = 'UNKNOWN',
 }
