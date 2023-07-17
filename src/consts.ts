151b3d0e3d3fbfd6df1c6569286e4cc3334affbd
Alexey Chernykh <alexeyshockman@gmail.com>
2023-07-17 13:15:01 +0300

    add 404-page, header & footer component

--- src/consts.ts
+++ src/consts.ts
@@ -1,13 +1,13 @@
 export const NUMBERS_OF_OFFERS = 5;
 
-export enum AppRoute {
+export const enum AppRoute {
 	Main = '/',
 	Login = '/login',
 	Favorites = '/favorites',
 	Offer = '/offer/:id',
 }
 
-export enum AuthorizationStatus {
+export const enum AuthorizationStatus {
 	Auth = 'AUTH',
 	NoAuth = 'NO_AUTH',
 	Unknown = 'UNKNOWN',
--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,11 +1,29 @@
 import {Link} from 'react-router-dom';
 
-function NotFoundPage (): JSX.Element {
+import styles from './not-found-page.module.css';
+import Header from '../../components/header/header';
+import {AppRoute} from '../../consts';
+import Footer from '../../components/footer/footer';
+
+function NotFoundPage(): JSX.Element {
 	return (
-		<div className='.notFoundPage'>
-			<h1>404. Page not found.</h1>
-			<p>try another page</p>
-			<Link to={'/'}>to main page</Link>
+		<div className="page page--favorites-empty">
+			<Header/>
+			<main className="page__main page__main--favorites page__main--favorites-empty">
+				<div className="page__favorites-container container">
+					<section className="favorites favorites--empty">
+						<h1 className="visually-hidden">404 Page not found</h1>
+						<div className={`${styles.wrapper} favorites__status-wrapper`}>
+							<b className="favorites__status">404 - Page not found</b>
+							<p className="favorites__status-description">
+								Try another page
+							</p>
+							<Link to={AppRoute.Main} className={`${styles.link}`}>To main page</Link>
+						</div>
+					</section>
+				</div>
+			</main>
+			<Footer />
 		</div>
 	);
 }
